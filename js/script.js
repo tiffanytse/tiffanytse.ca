98501f3b4a0357fbe7dcf80996be3c27355a2793
Tiffany Tse <ttse@coldfrontlabs.ca>
2015-08-10 16:52:33 -0400

    feat(js): add js for smooth scroll and update styling and layouts

--- js/script.js
+++ js/script.js
@@ -55,5 +55,21 @@ $(document).ready(function() {
       offset: 600
     });
 
+    // Smooth Scroll
+    $('a[href^="#"]').on('click', function(event) {
+
+        var target = $( $(this).attr('href') );
+
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+
+
 
 });
+
+
